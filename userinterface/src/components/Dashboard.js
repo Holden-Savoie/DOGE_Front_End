723e4b0beaf2e0d1c494540c463a2bbaf951581a
Taylor Cyr <michaelcyr79@yahoo.com>
2021-05-20 21:58:34 -0500

    Adjustments to scalability and line markers

--- userinterface/src/components/Dashboard.js
+++ userinterface/src/components/Dashboard.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import * as Constants from './constants';
 import dogerocketboi from "../imgs/doge-rocket-boi.png"
-import space from "../imgs/empty-space.jpg"
 import moon from "../imgs/moon.png"
 import "../styles/logStyles.css"
 
@@ -26,19 +25,19 @@ export default function Dashboard(props) {
                 setPrice(res.data.price)
             })
     }
-    let p=.3
     return (
         <div id="space">
-            <h1><em>Dogecoin Price Tracker</em></h1>
-            <div>
-            <h1 id="price">{price}</h1>
-            <button class="launch-button">To the Moon!</button>  
-            <img id="doge" style={{bottom: (p*100-15)+"vh", left: (p*100-20)+"vw"}}
+            <div id="titleInfo">
+                <h1><em>Dogecoin Price Tracker</em></h1>
+                <h1 id="price">{"$"+price}</h1>
+            </div>
+            <div className="line" id="one"><h3>$1.00</h3><hr/></div>
+            <div className="line" id="threeQuarters"><h3>$0.75</h3><hr/></div>
+            <div className="line" id="half"><h3>$0.50</h3><hr/></div>
+            <div className="line" id="oneQuarter"><h3>$0.25</h3><hr/></div>
+            <img id="doge" style={{top: (100-price*100+2.5)+"vh", left: (price>.2)?(price*100-30)+"%":"-10vw"}}
             src={dogerocketboi} alt="dogerocketboi.png" ></img>
-            </div>
-            <div id="moon">
-            <img src={moon} alt="moon.png" width="275vw"></img>
-            </div>
+            <img id="moon" src={moon} alt="moon.png"></img>
         </div>
         
     );
