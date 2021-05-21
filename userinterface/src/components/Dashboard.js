116458c0628fb277f64cd71c8525975fdf3c3ed6
troy-martin1 <tjmartin011@yahoo.com>
2021-05-20 21:23:38 -0500

    05/20/21

--- userinterface/src/components/Dashboard.js
+++ userinterface/src/components/Dashboard.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import * as Constants from './constants';
 import dogerocketboi from "../imgs/doge-rocket-boi.png"
 import space from "../imgs/empty-space.jpg"
+import moon from "../imgs/moon.png"
 import "../styles/logStyles.css"
 
 export default function Dashboard(props) {
@@ -25,15 +26,18 @@ export default function Dashboard(props) {
                 setPrice(res.data.price)
             })
     }
-
+    let p=.3
     return (
         <div id="space">
             <h1><em>Dogecoin Price Tracker</em></h1>
             <div>
             <h1 id="price">{price}</h1>
             <button class="launch-button">To the Moon!</button>  
-            <img id="doge" style={{bottom: (price*100-15)+"vh", left: (price*100-20)+"vw"}}
+            <img id="doge" style={{bottom: (p*100-15)+"vh", left: (p*100-20)+"vw"}}
             src={dogerocketboi} alt="dogerocketboi.png" ></img>
+            </div>
+            <div id="moon">
+            <img src={moon} alt="moon.png" width="275vw"></img>
             </div>
         </div>
         
